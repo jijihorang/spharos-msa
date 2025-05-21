cc6769dba59fd1803886343fbaa5909630effb22
jijihorang <jiho7213@gmail.com>
2025-05-21 11:26:25 +0900

    feat: feed-read-service

---
+++ feed-reed-service/src/main/java/com/kafka/feed_reed_service/FeedReedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.feed_reed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedReedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedReedServiceApplication.class, args);
+	}
+
+}
---
+++ feed-reed-service/src/test/java/com/kafka/feed_reed_service/FeedReedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.feed_reed_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedReedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
